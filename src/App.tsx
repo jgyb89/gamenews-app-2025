f023e8decc59f75b36fee3d792d86fc5437e2d3c
James Green <jgreen.gus@gmail.com>
2024-12-06 15:06:22 -0500

    Sign-up page initial setup
    Edited App.tsx to fix Routes bug from preventing sign-up page from showing up.
    
    Add assets for images/icons.

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,15 @@ const App = () => {
     <main className='flex h-screen'>
       <Routes>
         {/* public routes */}
-        <Route element={<AuthLayout />} />
-        <Route path="/sign-in" element={<SigninForm />} />
-        <Route path="/sign-up" element={<SignupForm />} />
-       
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
         {/* private routes */}
-        <Route element={<RootLayout />} />
-        <Route index element={<Home />}/>
-        
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   )
